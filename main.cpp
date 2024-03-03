46d3ed0b024d1d0dad31133c383bba3a17002753
TheBetterDark@TheBetterDark <91607604+TheBetterDark@users.noreply.github.com>
2024-03-03 22:00:33 +0000

    Improved array and added playerMove function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 
 void drawBoard(char *spaces);
-void playerMove(char *spaces, char player);
-void computerMove(char *spaces, char computer);
-bool checkWinner(char *spaces, char player, char computer);
+void playerMove(char *spaces, char playerCharacter);
+void computerMove(char *spaces, char computerCharacter);
+bool checkForWinner(char *spaces, char playerCharacter, char computerCharacter);
 bool checkTie(char *spaces);
 
 int main()
 {
     char spaces[9];
-    for (int i = 1; i <= 9; i++)
-    {
-        spaces[i] = '-';
-    }
+    std::fill(std::begin(spaces), std::end(spaces), '-');
 
     char playerCharacter = 'X';
     char computerCharacter = 'O';
     bool gameActive = true;
 
     drawBoard(spaces);
+
+    while (gameActive)
+    {
+        playerMove(spaces, playerCharacter);
+        drawBoard(spaces);
+        computerMove(spaces, computerCharacter);
+        drawBoard(spaces);
+    }
 
     return 0;
 };
@@ -37,13 +42,40 @@ void drawBoard(char *spaces)
     std::cout << "     |     |     " << '\n';
 }
 
-void playerMove(char *spaces, char player)
+void playerMove(char *spaces, char playerCharacter)
+{
+    int number;
+    do
+    {
+        std::cout << "Enter a position to place a marker (1-9): ";
+        std::cin >> number;
+
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            std::cout << "Invalid input! Please enter a number.\n";
+            continue;
+        }
+
+        number--;
+
+        if (number >= 0 && number < 9 && spaces[number] == '-')
+        {
+            spaces[number] = playerCharacter;
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid position, please enter a number between 1 and 9 where the space is not already occupied.\n";
+        }
+    } while (true);
+}
+
+void computerMove(char *spaces, char computeCharacterr)
 {
 }
-void computerMove(char *spaces, char computer)
-{
-}
-bool checkWinner(char *spaces, char player, char computer)
+bool checkForWinner(char *spaces, char playerCharacter, char computerCharacter)
 {
     return 0;
 }
